ccd9336eee75557855e0322af4e9854f8d4bd280
Ben Norcombe <B.E.Norcombe@exeter.ac.uk>
2017-06-26 09:54:30 +0100

    Fix test failures

--- src/Factories/UserProviderFactory.php
+++ src/Factories/UserProviderFactory.php
@@ -3,6 +3,7 @@
 namespace Maenbn\OpenAmAuthLaravel\Factories;
 
 
+use Illuminate\Database\Eloquent\Model;
 use Maenbn\OpenAmAuth\Contracts\OpenAm;
 use Maenbn\OpenAmAuthLaravel\Providers\OpenAmUserProvider;
 
@@ -16,7 +17,11 @@ class UserProviderFactory
     public static function create(array $config, OpenAm $openAm)
     {
         $model = ModelFactory::create($config['eloquentUser']);
-        $userRepository = UserRepositoryFactory::create($config['eloquentUidName'], $model);
+        $modelForRepository = $model;
+        if(!$modelForRepository instanceof Model){
+            $modelForRepository = null;
+        }
+        $userRepository = UserRepositoryFactory::create($config['eloquentUidName'], $modelForRepository);
         $userMapper = UserMapperFactory::create($userRepository);
 
         return new OpenAmUserProvider($openAm, $model, $userMapper);
