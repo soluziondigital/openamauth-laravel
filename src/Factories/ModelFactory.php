c35489f6a84d417f7f7762802549ca7fb6b77575
Ben Norcombe <B.E.Norcombe@exeter.ac.uk>
2017-06-26 09:06:13 +0100

    Change model factory to expect the eloquent model as a string. Then new up that class and check it's a instance of eloquent

--- src/Factories/ModelFactory.php
+++ src/Factories/ModelFactory.php
@@ -9,13 +9,20 @@ use Maenbn\OpenAmAuthLaravel\OpenAmUser;
 class ModelFactory
 {
     /**
-     * @param Model|null $eloquentModel
-     * @return OpenAmUser|Model
+     * @param string|null $eloquentModel
+     * @return Model|OpenAmUser
+     * @throws \Exception
      */
-    public static function create(Model $eloquentModel = null)
+    public static function create($eloquentModel = null)
     {
         if(is_null($eloquentModel)){
             return new OpenAmUser();
+        }
+
+        $eloquentModel = new $eloquentModel;
+
+        if(!$eloquentModel instanceof Model){
+            throw new \Exception('Model provided is not an Eloquent Model');
         }
 
         return new $eloquentModel;
